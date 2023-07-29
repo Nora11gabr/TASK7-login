f61d93a5cb35a9c3e48e02a1e10a83451fbf9590
Nora11gabr <noragabr48@gmail.com>
2023-07-29 23:59:58 +0300

    first commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,50 +4,55 @@ import '../widgets/button.dart';
 import '../widgets/textformfield.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+  HomePage({super.key});
+  GlobalKey<FormState> formstate = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Color(0xff2b475e),
-      body: ListView(
-        padding: EdgeInsets.all(8),
-        children: [
-          SizedBox(
-            height: 280,
-          ),
-          customtextfeild(
-            isvalid: (value) {
-              if (value.length < 10) {
-                return "Email is too short";
-              }
-            },
-            labeltxt: "Email",
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          customtextfeild(
-            isvalid: (value) {
-              if (value.length < 8) {
-                return "Password is too short";
-              }
-            },
-            labeltxt: "Password",
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          CustomButton(
-              ontap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(8),
+          children: [
+            SizedBox(
+              height: 280,
+            ),
+            customtextfeild(
+              isvalid: (value) {
+                if (value.length < 10) {
+                  return "Email is too short";
+                }
               },
-              tex: "Log in"),
-        ],
+              labeltxt: "Email",
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            customtextfeild(
+              isvalid: (value) {
+                if (value.length < 8) {
+                  return "Password is too short";
+                }
+              },
+              labeltxt: "Password",
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            CustomButton(
+                ontap: () {
+                  var formdata = formstate.currentState;
+                  if (formdata!.validate()) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  } else {
+                    print("error");
+                  }
+                },
+                tex: "Log in"),
+          ],
+        ),
       ),
     );
   }
